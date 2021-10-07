605f9f49f364b963583f1c48d00551b9cbd5f8c1
iamrish <rishabhmalhotra57@gmail.com>
2021-10-07 23:38:53 +0530

    updated the graphical abstract image

--- src/components/Paper.js
+++ src/components/Paper.js
@@ -1,11 +1,13 @@
 import React from "react";
-import graphicalAbstarct from "../images/graphical_abstract.PNG";
+import graphicalAbstarct from "../images/graphical_abstract.jpg";
 
 const Paper = () => {
   return (
-    <div className="ui row">
+    <div className="ui row centered">
       <div className="sixteen wide column" style={{ textAlign: "center" }}>
         <h1>Graphical Abstract</h1>
+      </div>
+      <div className="ten wide column">
         <img
           src={graphicalAbstarct}
           alt="Graphical Abstract"
