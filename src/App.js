1586eafffd0a2d2842f2460be46d151189f27cc7
iamrish <rishabhmalhotra57@gmail.com>
2022-06-15 13:34:33 +0530

    june update and updated history card

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import EntropyToggler from "./components/EntropyToggler";
 import entropies from "./data/entropies";
 // console.log(entropies);
 
-const lastUpdatedDate = "08-05-22";
+const lastUpdatedDate = "15-06-22";
 
 const App = () => {
   const [countrySelection, onCountrySelect] = useState("India");
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,16 +1,56 @@
-import React from "react";
+// import React from "react";
+// import CasesLeftGraph from "./CasesLeftGraph";
+
+// const Cards = (country) => {
+//   console.log(country.country);
+//   const history = country.country.map((c) => {
+//     return (
+//       <div className="centered seven wide column">
+//         <CasesLeftGraph country={c} labels={c.predDates} />
+//       </div>
+//     );
+//   });
+//   return <>{history}</>;
+// };
+
+// export default Cards;
+import React, { useState } from "react";
+import { cloneDeep } from "lodash";
+
 import CasesLeftGraph from "./CasesLeftGraph";
+import Dropdown from "./Dropdown";
 
 const Cards = (country) => {
-  console.log(country.country);
-  const history = country.country.map((c) => {
-    return (
-      <div className="centered seven wide column">
-        <CasesLeftGraph country={c} labels={c.predDates} />
+  console.log(country);
+  const [month, selectMonth] = useState("Oct '21");
+  let monthList = [];
+  let history;
+  for (const c of country.country) {
+    if (c.month === month) history = cloneDeep(c);
+    monthList.push(c.month);
+  }
+
+  return (
+    <>
+      <div className="ui row">
+        <div
+          className="centered four wide column"
+          style={{ paddingTop: "20px" }}
+        >
+          <Dropdown
+            selection={month}
+            label={"Select the month"}
+            onSelect={selectMonth}
+            options={monthList}
+          />
+        </div>
       </div>
-    );
-  });
-  return <>{history}</>;
+
+      <div className="centered seven wide column">
+        <CasesLeftGraph country={history} labels={history.predDates} />
+      </div>
+    </>
+  );
 };
 
 export default Cards;
